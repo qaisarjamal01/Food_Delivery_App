37fe2cf9a75dbf539bbf6313a1e90a33524c5f7d
qaisarjamal01 <qaisarjamaluop@gmail.com>
2025-07-18 17:47:40 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:food_delivery_app/login_page.dart';
+import 'package:food_delivery_app/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
